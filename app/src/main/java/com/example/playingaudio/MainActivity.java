72c5afee3c10978225fa7f9d2c47fdefa3a6d483
”Artem <scgame2231@gmail.com>
2020-05-03 00:39:09 +0300

    gogo

--- app/src/main/java/com/example/playingaudio/MainActivity.java
+++ app/src/main/java/com/example/playingaudio/MainActivity.java
@@ -1,44 +1,50 @@
 package com.example.playingaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 
+import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.Image;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
-    Button button;
+    ImageView playPauseIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.playButton);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mediaPlayer.isPlaying()) {
-                    pause();
-                } else {
-                    play();
-                }
-            }
-        });
+        playPauseIcon = findViewById(R.id.goSongIcon);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+
+
     }
 
-    public void play() {
-        mediaPlayer.start();
-        button.setText("Pause");
+    public void previous(View view) {
     }
 
-    public void pause() {
-        mediaPlayer.pause();
-        button.setText("Play");
+    public void play(View view) {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            playPauseIcon.setImageResource(R.drawable.ic_play_arrow_blue_24dp);
+        } else {
+            mediaPlayer.start();
+            playPauseIcon.setImageResource(R.drawable.ic_pause_blue_24dp);
+        }
+
     }
-}
+        public void next (View view) {
+        }
+    }
